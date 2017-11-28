f4a4bb72beb82104aad390772bca174f5edb6b45
tomonarifeehan <feehan.tomonari@gmail.com>
2017-11-28 16:24:09 -0500

    Added run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=14
+#PBS -l walltime=12:00:00
+#PBS -q joe
+#PBS -N test
+#PBS -o stdout
+#PBS -e stderr
+
+cd $PBS_O_WORKDIR
+source  /gpfs/pace1/project/chbe-medford/medford-share/envs/espresso-5.1.r11289-pybeef_ase3.13_cust_esp
+
+
+python calc.py
